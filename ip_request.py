6cb3cbfb81218058fb6002c5b9b055b4e1591866
Parag Pallav Singh <40052830+paragpallavsingh@users.noreply.github.com>
2022-01-15 18:42:33 +0530

    Add files via upload

---
+++ ip_request.py
@@ -0,0 +1,33 @@
+'''
+A few weeks ago #google has released the IP ranges for Googlebot allowing #SEO to more easily verify if a server request is really from Google or just someone impersonating it.
+'''
+
+import ipaddress
+import requests
+import pandas as pd
+import base64
+import streamlit as st
+api = "https://developers.google.com/search/apis/ipranges/googlebot.json"
+
+def getIPfromPrefix(ipPrefix):
+	lst_ips = [str(ip) for ip in ipaddress. IPv4Network( ipPrefix)]
+	return lst_ips
+r = requests.get(api).json()
+
+prefixLst = [resp['ipv4Prefix'] for resp in r["prefixes"] if 'ipv4Prefix' in resp]
+df_pref_list = pd.DataFrame(prefixLst)
+
+full_lst = [getIPfromPrefix(i) for i in prefixLst]
+df_full_lst = pd.DataFrame(full_lst)
+
+#Streamlit start design
+st.set_page_config(page_title="List of IP")
+st.title("***Word Counter***", anchor=None)
+
+st.table(df_full_lst)
+csv = df_full_lst.to_csv()
+b64 = base64.b64encode(csv.encode()).decode()
+st.markdown('### **⬇️ Download output CSV File **')
+href = f"""<a href="data:file/csv;base64,{b64}">Download CSV File</a> (Right-click and save as "filename.csv". Don't left-click)"""
+st.markdown(href, unsafe_allow_html=True)
+
